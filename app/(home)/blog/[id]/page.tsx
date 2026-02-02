56d00ec52cbb6a26799d6d7eaafe5a65e0b47956
SchoolingSocial5 <schoolingsocial5@gmail.com>
2026-02-02 16:17:11 +0100

    Updating contents

--- app/(home)/blog/[id]/page.tsx
+++ app/(home)/blog/[id]/page.tsx
@@ -33,11 +33,33 @@ export default function Blogs() {
           <Image
             src={blogForm.picture ? String(blogForm.picture) : '/images/page-header.jpg'}
             sizes="100vw"
-            className="h-auto w-full object-contain max-w-[600px]"
+            className="h-auto w-full object-contain max-w-[600px] mb-5"
             width={0}
             height={0}
             alt="real"
           />
+
+          <div className="text">
+            <div
+              className="z-10 text-black text-[20px] mb-3 block font-bold"
+            >
+              {blogForm.title}
+            </div>
+            <div className="flex items-center ">
+              <div className="text mr-1  text-[var(#C1C1C4)] text-[12px]">
+                BY
+              </div>
+              <span
+                className="  text-black text-[12px] mr-4 font-bold"
+              >
+                {blogForm.author}
+              </span>
+              <div className="text-black flex">
+                <i className="bi bi-calendar mr-2"></i>
+                <div className="text-[var(#C1C1C4)]">{formatDate(String(blogForm.createdAt))} </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
